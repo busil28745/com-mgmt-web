2b0339a33ab47e877936c65f7c3ea4f9028d4c71
rimtae <zmstnv01@gmail.com>
2022-06-15 23:59:09 +0900

    이메일전송2

---
+++ board-app/src/components/SendEmailComponent.jsx
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import BoardService from '../service/BoardService';
+
+class SendEmailComponent extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+            adress : '',
+            title : '',
+            content : '',
+
+        }
+
+        this.changeAdressHandler = this.changeAdressHandler.bind(this);
+        this.changeTitleHandler = this.changeTitleHandler.bind(this);
+        this.changeContentHandler = this.changeContentHandler.bind(this);
+
+        this.sendEmail = this.sendEmail.bind(this);
+    }
+
+
+    changeAdressHandler = (event) => {
+        this.setState({ adress : event.target.value});
+    }
+
+    changeTitleHandler = (event) => {
+        this.setState({ title : event.target.value});
+    }
+
+
+    changeContentHandler = (event) => {
+        this.setState({content: event.target.value});
+    }
+
+
+
+    sendEmail = (event) => {
+        event.preventDefault();
+        let email = {
+                    adress: this.state.adress,
+                    title: this.state.title,
+                    content: this.state.content,
+    
+        };
+
+         console.log("email => "+ JSON.stringify(email));
+        BoardService.sendEmail(email).then(res => {
+            this.props.history.push('/mail/send/');
+        });
+
+    }
+
+    cancel() {
+        this.props.history.push('/board');
+    }
+
+    
+
+    render() {
+        return (
+            <div>
+                <div className = "container">
+                    <div className = "row">
+                        <div className = "card col-md-6 offset-md-3 offset-md-3">
+                        <h3 className="text-center"> 메일 작성 </h3>
+                            <div className = "card-body">
+                                <form>
+                                    <div className = "form-group">
+                                        <label> 이메일 주소 </label>
+                                        <input type="text" placeholder="adress" name="adress" className="form-control"
+                                        value={this.state.adress} onChange={this.changeAdressHandler}/>
+                                    </div>
+                                    <div className = "form-group">
+                                        <label> 제목 </label>
+                                        <input type="text" placeholder="title" name="title" className="form-control"
+                                               value={this.state.title} onChange={this.changeTitleHandler}/>
+                                    </div>
+                                    <div className = "form-group">
+                                        <label> 내용 </label>
+                                        <textarea placeholder="content" name="content" className="form-control" 
+                                        value={this.state.content} onChange={this.changeContentHandler}/>
+                                    </div>                    
+
+                                    <button className="btn btn-success" onClick={this.sendEmail}> Send </button>
+                                    <button className="btn btn-danger" onClick={this.cancel.bind(this)} style={{marginLeft:"10px"}}>Cancel</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
+}
+
+export default SendEmailComponent;
